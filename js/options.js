b6b67d0c31ac441f1da4ee0a7231446955b317bb
VirtualTakagi <benki@live.jp>
2019-08-07 18:52:51 +0900

    options.js edited

--- js/options.js
+++ js/options.js
@@ -19,15 +19,24 @@ $(function(){
 	if (localStorage["notifTime1"]) {
 		var time1 = localStorage["notifTime1"]
 		$("#notifTime1 option[value=" + time1 + "]").attr("selected", true);
+	} else {
+		var defaultTime1 = 60;
+		$("#notifTime1 option[value="+ defaultTime1 + "]").attr("selected", true);
 	}
 
 	if (localStorage["notifTime2"]) {
 		var time2 = localStorage["notifTime2"]
 		$("#notifTime2 option[value=" + time2 + "]").attr("selected", true);
+	} else {
+		var defaultTime2 = 30;
+		$("#notifTime2 option[value=" + defaultTime2 + "]").attr("selected", true);
 	}
 
 	if (localStorage["notifTime3"]) {
 		var time3 = localStorage["notifTime3"]
 		$("#notifTime3 option[value=" + time3 + "]").attr("selected", true);
+	} else {
+		var defaultTime3 = 10;
+		$("#notifTime3 option[value=" + defaultTime3 + "]").attr("selected", true);
 	}
 });
