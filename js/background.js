8d46947702ae68e86cd3bdd0490638edfb279e81
VirtualTakagi <benki@live.jp>
2019-08-08 19:16:57 +0900

    deleted comment

--- js/background.js
+++ js/background.js
@@ -2,11 +2,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 	var notifTime = new Array();
 
-	//アラーム作成
 	chrome.alarms.create(request.text, { "periodInMinutes": 1 });
 	console.log("created alarm: " + request.text);
 
-	// 1分間隔で通知時刻と現在時刻の比較を行う
 	chrome.alarms.onAlarm.addListener(function (alarm) {
 
 		var infoArray = alarm.name.split(",");
@@ -14,12 +12,10 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		var startTime = infoArray[1];
 		var remain = getTimeLeft(startTime);
 
-		//LocalStorageから通知時間を読み込む
 		notifTime[0] = localStorage["notifTime1"] ? localStorage["notifTime1"] : 191844000;
 		notifTime[1] = localStorage["notifTime2"] ? localStorage["notifTime2"] : 191844000;
 		notifTime[2] = localStorage["notifTime3"] ? localStorage["notifTime3"] : 191844000;
 
-		//開始までの時間が通知時間と一致したら通知を行う
 		if((notifTime[2] == remain)
 				|| (notifTime[1] == remain)
 				|| (notifTime[0] == remain)){
@@ -29,7 +25,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		}
 	});
 
-	//通知処理
 	function dispNotification(title, remain){
 
     	var notifString = new Date().getMinutes();
@@ -47,19 +42,11 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 	}
 
-	//通知判定に使用する残り時間を算出する
 	function getTimeLeft(startTime){
 
-		//開始時刻の文字列をDate型に変換
 		var start = new Date(startTime);
-
-		//現在時刻を取得
 		var now = new Date();
-
-		//開始時刻と現在時刻の差を秒へ変換
 		var duration = (start.getTime() - now.getTime()) / 1000;
-
-		//分を算出
 		var remaining = Math.floor(duration / 60);
 
 		return remaining;
--- js/options.js
+++ js/options.js
@@ -1,27 +1,23 @@
-//保存ボタンがクリックされたらLocalStorageに保存する
 $('#save').on('click', function(){
-	//ページ遷移
 	window.location.href = "../popup.html";
 
-	//LocalStorageに通知時間を保存
 	localStorage["notifTime1"] = $('#notifTime1').val();
 	localStorage["notifTime2"] = $('#notifTime2').val();
 	localStorage["notifTime3"] = $('#notifTime3').val();
 });
 
-//キャンセルがクリックされたらページ遷移する
 $('#cancel').on('click', function() {
 	window.location.href = "../popup.html";
 });
 
 $(function(){
-	//オプション画面の初期値を設定する
 	if (localStorage["notifTime1"]) {
 		var time1 = localStorage["notifTime1"]
 		$("#notifTime1 option[value=" + time1 + "]").attr("selected", true);
 	} else {
 		var defaultTime1 = 60;
 		$("#notifTime1 option[value="+ defaultTime1 + "]").attr("selected", true);
+		localStorage["notifTime1"] = $('#notifTime1').val();
 	}
 
 	if (localStorage["notifTime2"]) {
@@ -30,6 +26,7 @@ $(function(){
 	} else {
 		var defaultTime2 = 30;
 		$("#notifTime2 option[value=" + defaultTime2 + "]").attr("selected", true);
+		localStorage["notifTime2"] = $('#notifTime2').val();
 	}
 
 	if (localStorage["notifTime3"]) {
@@ -38,5 +35,6 @@ $(function(){
 	} else {
 		var defaultTime3 = 10;
 		$("#notifTime3 option[value=" + defaultTime3 + "]").attr("selected", true);
+		localStorage["notifTime3"] = $('#notifTime3').val();
 	}
 });
